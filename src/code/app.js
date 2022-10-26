6af6ec147112546d9a9ff90c58860885e9ecc7ff
steven <alvaradosteven315@gmail.com>
2022-10-26 11:59:13 -0500

    adding dark mode

--- src/code/app.js
+++ src/code/app.js
@@ -1,117 +1,131 @@
 //reset btn
 
-const reset = document.querySelector(".btn-remove-shadow");
+const reset = document.querySelector('.btn-remove-shadow')
 
 //box shadow variables
-const inputIsInset = document.querySelector("#inset");
-const inputRangeHorizontal = document.querySelector("#horizontal");
-const inputRangeVertical = document.querySelector("#vertical");
-const inputRangeBlur = document.querySelector("#blur");
-const inputRangeSpread = document.querySelector("#spread");
-const inputColorShadow = document.querySelector("#color");
-const inputColorOpacity = document.querySelector("#color-opacity");
+const inputIsInset = document.querySelector('#inset')
+const inputRangeHorizontal = document.querySelector('#horizontal')
+const inputRangeVertical = document.querySelector('#vertical')
+const inputRangeBlur = document.querySelector('#blur')
+const inputRangeSpread = document.querySelector('#spread')
+const inputColorShadow = document.querySelector('#color')
+const inputColorOpacity = document.querySelector('#color-opacity')
 
 //select all box shadow sliders
-const inputsShadow = document.querySelectorAll(".input-range");
+const inputsShadow = document.querySelectorAll('.input-range')
 
 //box dimension variables
-const inputWidth = document.querySelector("#width-range");
-const inputHeight = document.querySelector("#height-range");
-const inputBorderRadius = document.querySelector("#border-radius");
-const backgroundColor = document.querySelector("#background-color");
+const inputWidth = document.querySelector('#width-range')
+const inputHeight = document.querySelector('#height-range')
+const inputBorderRadius = document.querySelector('#border-radius')
+const backgroundColor = document.querySelector('#background-color')
 
 //variable of the element to modify
-const square = document.querySelector(".square");
+const square = document.querySelector('.square')
 
 // to handle the modal
 
-const modal = document.querySelector(".modal");
-const openModalBtn = document.querySelector("#show-code");
-const closeModalBnt = document.querySelector(".closemodal");
-const overlay = document.querySelector(".overlay");
+const modal = document.querySelector('.modal')
+const openModalBtn = document.querySelector('#show-code')
+const closeModalBnt = document.querySelector('.closemodal')
+const overlay = document.querySelector('.overlay')
 
 //to select the display to show the code
-const display = document.querySelector(".display");
+const display = document.querySelector('.display')
 
 //make shadow
 
 inputsShadow.forEach((input) => {
-  input.addEventListener("input", makeShadow);
-});
+  input.addEventListener('input', makeShadow)
+})
 
 function makeShadow() {
-  let insetShadow = inputIsInset.checked;
+  let insetShadow = inputIsInset.checked
 
-  let shadowInX = inputRangeHorizontal.value;
-  let shadowInY = inputRangeVertical.value;
-  let blurRadius = inputRangeBlur.value;
-  let spread = inputRangeSpread.value;
-  let shadowColor = inputColorShadow.value;
+  let shadowInX = inputRangeHorizontal.value
+  let shadowInY = inputRangeVertical.value
+  let blurRadius = inputRangeBlur.value
+  let spread = inputRangeSpread.value
+  let shadowColor = inputColorShadow.value
 
   //we create a variable to verify if the inset checkbox is selected
   let boxShadowOutput = insetShadow
     ? `inset ${shadowInX}px ${shadowInY}px ${blurRadius}px ${spread}px ${shadowColor}`
-    : `${shadowInX}px ${shadowInY}px ${blurRadius}px ${spread}px ${shadowColor}`;
+    : `${shadowInX}px ${shadowInY}px ${blurRadius}px ${spread}px ${shadowColor}`
 
   //we assign the styles of the square with the generated code
 
-  square.style.boxShadow = `${boxShadowOutput}`;
+  square.style.boxShadow = `${boxShadowOutput}`
 
   //then, the code is showing to the display
-  display.innerHTML = `box-shadow: ${boxShadowOutput}`;
+  display.innerHTML = `box-shadow: ${boxShadowOutput};`
 }
 
 //box properties
 
-inputWidth.addEventListener("input", (e) => {
-  const value = e.target.value;
-  square.style.width = `${value}px`;
-});
+inputWidth.addEventListener('input', (e) => {
+  const value = e.target.value
+  square.style.width = `${value}px`
+})
 
-inputHeight.addEventListener("input", (e) => {
-  const value = e.target.value;
-  square.style.height = `${value}px`;
-});
+inputHeight.addEventListener('input', (e) => {
+  const value = e.target.value
+  square.style.height = `${value}px`
+})
 
-inputBorderRadius.addEventListener("input", (e) => {
-  const value = e.target.value;
-  square.style.borderRadius = `${value}px`;
-});
+inputBorderRadius.addEventListener('input', (e) => {
+  const value = e.target.value
+  square.style.borderRadius = `${value}px`
+})
 
-backgroundColor.addEventListener("input", (e) => {
-  const value = e.target.value;
-  square.style.backgroundColor = value;
-});
+backgroundColor.addEventListener('input', (e) => {
+  const value = e.target.value
+  square.style.backgroundColor = value
+})
 
 //to open and close the modal
 
 const openModal = () => {
-  modal.classList.remove("hidden");
-  overlay.classList.remove("hidden");
-};
+  modal.classList.remove('hidden')
+  overlay.classList.remove('hidden')
+}
 
 const closeModal = () => {
-  modal.classList.add("hidden");
-  overlay.classList.add("hidden");
-};
+  modal.classList.add('hidden')
+  overlay.classList.add('hidden')
+}
 
-inputIsInset.addEventListener("change", (e) => {
-  makeShadow();
-});
+inputIsInset.addEventListener('change', (e) => {
+  makeShadow()
+})
 
-openModalBtn.addEventListener("click", openModal);
+openModalBtn.addEventListener('click', openModal)
 
-closeModalBnt.addEventListener("click", closeModal);
-overlay.addEventListener("click", closeModal);
+closeModalBnt.addEventListener('click', closeModal)
+overlay.addEventListener('click', closeModal)
 
 //reset
 
-reset.addEventListener("click", () => {
-  inputIsInset.checked = false;
-  inputRangeHorizontal.value = 0;
-  inputRangeVertical.value = 0;
-  inputRangeBlur.value = 0;
-  inputRangeSpread.value = 0;
-  inputColorShadow.value = "#ffffff";
-  makeShadow();
-});
+reset.addEventListener('click', () => {
+  inputIsInset.checked = false
+  inputRangeHorizontal.value = 0
+  inputRangeVertical.value = 0
+  inputRangeBlur.value = 0
+  inputRangeSpread.value = 0
+  inputColorShadow.value = '#ffffff'
+  makeShadow()
+})
+
+//dark mode
+
+const toggle = document.querySelector('.dark-mode')
+
+toggle.addEventListener('click', () => {
+  if (document.body.classList.contains('dark')) {
+    document.body.classList.remove('dark')
+    toggle.innerHTML = '🌞'
+  } else {
+    document.body.classList.add('dark')
+    toggle.innerHTML = '🌑'
+  }
+})
